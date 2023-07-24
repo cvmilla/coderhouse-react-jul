2202800e8673f8639445497f5d0fb2944fc0e8b9
camilla.coelho <cvmilladev@gmail.com>
2023-07-24 17:56:39 -0300

    atualiza navbar em components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import logo from './logo-books.png';
 import './App.css';
-import './components/NavBar'
+import NavBar from './components/NavBar';
 
 function App() {
   return (    
     <div className="App">    
       <header className="App-header">
-        <nav>
-            <div class="logo">
-                <a href="#">
-                    <img src={logo} alt="Livraria Coder"/>
-                </a>
-            </div>
-            <ul class="navbar">
-                <li><a href="#">Início</a></li>
-                <li><a href="#">Quem Somos</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Estoque</a></li>
-                <li><a href="#">Contato</a></li>
-            </ul>
-            <div class="search">
-                <input type="text" placeholder="Pesquisar..."/>
-                <button type="button">Buscar</button>
-            </div>
-        </nav>
+        {NavBar}
       </header>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,27 @@
-import './NavBar.css'
+import './NavBar.css';
+import logo from './logo-books.png';
+
+function NavBar() {
+    return(
+        <nav>
+        <div class="logo">
+            <a href="#">
+                <img src={logo} alt="Livraria Coder"/>
+            </a>
+        </div>
+        <ul class="navbar">
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Quem Somos</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Estoque</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+        <div class="search">
+            <input type="text" placeholder="Pesquisar..."/>
+            <button type="button">Buscar</button>
+        </div>
+    </nav> 
+    )
+}
+
+export default NavBar();
